3c9cce26cdac7ee929853a5e49b7625b4edf85fc
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-18 17:47:17 +0530

    Update app.js

--- Selecting/app.js
+++ Selecting/app.js
@@ -9,3 +9,9 @@ const squareImages = document.getElementsByClassName("square")
 for (let image of squareImages) {
     image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg";
 }
+
+const links = document.querySelectorAll('p a');
+
+for (let link of links) {
+    console.log(link.href);
+}
