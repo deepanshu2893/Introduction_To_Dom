e0f5b83047c5b5a451d90826541da80ad20204c9
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-01-21 21:33:18 +0530

    dom events

--- DOM_Events/Event_Basics/app.js
+++ DOM_Events/Event_Basics/app.js
@@ -15,3 +15,24 @@ btn.onmouseenter = scream;
 document.querySelector("h1").onclick = function () {
     alert("you clicked the h1");
 }
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("mousedown", function () {
+    alert("Clicked")
+})
+
+
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout; 
+
+tasButton.addEventListener("click", () => { console.log("Shout") }, { once: true })
+tasButton.addEventListener("click", shout)
