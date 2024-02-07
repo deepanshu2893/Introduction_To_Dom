b87ecf046289e6afc6c590f13408c8b74cd2d9ea
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-07 11:23:10 +0530

    js

--- TheEventObject/app.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("button").addEventListener("click", function (evt) {
-    console.log(evt)
-})
-
-const input = document.querySelector("input");
-input.addEventListener("keydown", function (e) {
-    console.log(e);
-})
